f9d1d239a3567aa68ad007f60032f87c5e28ee8a
jhoun <jayhoun@gmail.com>
2017-01-10 18:17:08 -1000

    passed merge sort logic

--- modules/merge_sort.js
+++ modules/merge_sort.js
@@ -7,20 +7,55 @@ let right = [];
 
 if(lengthOfArr < 2){
   return;
-} else {
-  for (var i =0; i < lengthOfArr; i++){
-      if (numArr.indexOf(numArr[i]) < middle){
-        left.push(numArr[i]);
-      } else {
-        right.push(numArr[i]);
-      }
-  }
+}
+
+for (var i =0; i < lengthOfArr; i++){
+    if (numArr.indexOf(numArr[i]) < middle){
+      left.push(numArr[i]);
+    } else {
+      right.push(numArr[i]);
+    }
 }
 
 
-console.log('left: ', left);
-console.log('right: ', right);
+mergeSort(left);
+mergeSort(right);
+return merge(left,right, numArr);
+
 
 };
 
+function merge(left, right, numArr){
+
+  var lengthOfLeft = left.length;
+  var lengthOfRight = right.length;
+  var i = 0;
+  var j = 0;
+  var k = 0;
+
+  while(i < lengthOfLeft && j < lengthOfRight){
+    if (left[i] <= right[j]) {
+      numArr[k] = left[i];
+      i +=1;
+    } else{
+      numArr[k] = right[j];
+      j += 1
+    }
+    k +=1;
+  }
+  while(i < lengthOfLeft) {
+    numArr[k] = left[i];
+    i +=1;
+    k +=1;
+  }
+  while(j < lengthOfRight){
+    numArr[k] = right[j];
+    j +=1;
+    k +=1;
+  }
+  return numArr;
+}
+
+
+// mergeSort([2, 4, 1, 6, 8, 5, 3, 7]);
 
--- tests/merge_sort.spec.js
+++ tests/merge_sort.spec.js
@@ -5,9 +5,9 @@ const mergeSort = require('../modules/merge_sort');
 
 describe('Using merge to sort an array', function() {
   it('input array should be sorted in order', function() {
-    let numArr = [5, 1, 4, 2, 3, 6];
+    let numArr = [2, 4, 1, 6, 8, 5, 3, 7];
 
-    expect(mergeSort(numArr)).to.deep.equal([1, 2, 3, 4, 5, 6]);
+    expect(mergeSort(numArr)).to.deep.equal([1, 2, 3, 4, 5, 6, 7 , 8]);
   })
 
 })
